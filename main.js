ea470cd7cf50dc0aabb6480cc3615c83ac0fd4f6
Slawomir <srostkow@o2.pl>
2019-11-03 10:09:59 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('siema');
+const name = 'Maciek';
+const age = 31;
+console.log(name);
+console.log(age);
+
+console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+console.log('Witaj przybyszu');
