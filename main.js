e44e903f756b58c7cf874342583e55bc86134779
Slawomir <srostkow@o2.pl>
2019-11-19 10:02:52 +0100

    CSS modification and normalization

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ console.log(age);
 console.log(`Nazywam się ${name} i mam ${age} lat`);
 
 console.log('Witaj przybyszu');
+
+const about = document.querySelector('.about__paragraph--js');
+
+console.log(about.innerHTML);
+
+about.innerHTML = "Pozdro js";
+
+function calculate(x) {
+    x = x + 3;
+    console.log(x);
+    return x*7
+}
+
+calculate(1);
+console.log(calculate(2));
